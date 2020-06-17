a5464420b7761205a938597a366c6dd783579db3
alvinpgao <alvinpgao@tencent.com>
2020-06-17 13:55:01 +0800

    修改主页面样式

--- web/teaching-mgt-client/src/components/main.vue
+++ web/teaching-mgt-client/src/components/main.vue
@@ -1,87 +1,58 @@
 <template>
-  <a-layout id="components-layout-top-side" style="height:100%;width:100%;position:fixed">
-    <a-layout-header class="header">
-      <div class="logo"></div>
-      <a-menu
-        theme="dark"
-        mode="horizontal"
-        :default-selected-keys="['2']"
-        :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
+  <a-layout id="layout-responsive" style="position: fixed; width: 100%; height: 100%">
+    <a-layout-sider
+      breakpoint="lg"
+      collapsed-width="0"
+      @collapse="onCollapse"
+      @breakpoint="onBreakpoint"
+    >
+      <div class="logo" style="text-align: center"><h2>教务管理系统</h2></div>
+      <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
+        <a-menu-item key="1">
+          <a-icon type="user"/>
+          <span class="nav-text">nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="video-camera"/>
+          <span class="nav-text">nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <a-icon type="upload"/>
+          <span class="nav-text">nav 3</span>
+        </a-menu-item>
+        <a-menu-item key="4">
+          <a-icon type="user"/>
+          <span class="nav-text">nav 4</span>
+        </a-menu-item>
       </a-menu>
-    </a-layout-header>
-    <a-layout style="height:100%">
-      <a-layout-sider width="200px" style="background: #fff">
-        <a-menu
-          mode="inline"
-          :default-selected-keys="['1']"
-          :default-open-keys="['sub1']"
-          :style="{ height: '100%', borderRight: 0 }"
-        >
-          <a-sub-menu key="sub1">
-            <span slot="title">
-              <a-icon type="user" />subnav 1
-            </span>
-            <a-menu-item key="1">option1</a-menu-item>
-            <a-menu-item key="2">option2</a-menu-item>
-            <a-menu-item key="3">option3</a-menu-item>
-            <a-menu-item key="4">option4</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <span slot="title">
-              <a-icon type="laptop" />subnav 2
-            </span>
-            <a-menu-item key="5">option5</a-menu-item>
-            <a-menu-item key="6">option6</a-menu-item>
-            <a-menu-item key="7">option7</a-menu-item>
-            <a-menu-item key="8">option8</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub3">
-            <span slot="title">
-              <a-icon type="notification" />subnav 3
-            </span>
-            <a-menu-item key="9">option9</a-menu-item>
-            <a-menu-item key="10">option10</a-menu-item>
-            <a-menu-item key="11">option11</a-menu-item>
-            <a-menu-item key="12">option12</a-menu-item>
-          </a-sub-menu>
-        </a-menu>
-      </a-layout-sider>
-      <a-layout style="padding: 0 24px 24px">
-        <a-breadcrumb style="margin: 16px 0">
-          <a-breadcrumb-item>Home</a-breadcrumb-item>
-          <a-breadcrumb-item>List</a-breadcrumb-item>
-          <a-breadcrumb-item>App</a-breadcrumb-item>
-        </a-breadcrumb>
-        <a-layout-content
-          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-        >Content</a-layout-content>
-      </a-layout>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header :style="{ background: '#fff', padding: 0 }">
+
+      </a-layout-header>
+      <a-layout-content :style="{ margin: '24px 16px 0' }">
+        <div id="content" :style="{ padding: '24px', background: '#fff', minHeight: '360px', height: '100%' }">
+
+        </div>
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        <a-tag>数据库课设 @2020 Create By Aliver. Contact Me
+          <a-tag color="blue">aliver_len@qq.com</a-tag>
+        </a-tag>
+      </a-layout-footer>
     </a-layout>
   </a-layout>
 </template>
 <script>
-export default {
-  data() {
-    return {
-      collapsed: false
-    }
+  export default {
+
   }
-};
 </script>
 
 <style>
-#components-layout-top-side .logo {
-  width: 120px;
-  height: 31px;
-  /* background: rgba(255, 255, 255, 0.2); */
-  margin: 16px 28px 16px 0;
-  float: left;
-  text-align: center;
-  /* font-size: larger; */
-  color: white;
-}
+  #layout-responsive .logo {
+    height: 32px;
+    background: rgba(255, 255, 255, 1);
+    margin: 16px;
+  }
 </style>
--- web/teaching-mgt-client/src/main.js
+++ web/teaching-mgt-client/src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { DatePicker, Button, Layout, Menu, Breadcrumb, Icon, Form, Input, Row, Col, Radio, Modal } from 'ant-design-vue'
+import { DatePicker, Button, Layout, Menu, Breadcrumb, Icon, Form, Input, Row, Col, Radio, Modal, Tag } from 'ant-design-vue'
 import axios from "axios"
 import store from "./router/store"
 
@@ -14,20 +14,21 @@ Vue.prototype.$axios = axios;
 
 // axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
-axios.defaults.baseURL = '/api';
+axios.defaults.baseURL = '/api'
 
-Vue.use(DatePicker);
-Vue.use(Button);
-Vue.use(Layout);
-Vue.use(Menu);
-Vue.use(Breadcrumb);
-Vue.use(Icon);
-Vue.use(Form);
-Vue.use(Input);
-Vue.use(Row);
-Vue.use(Col);
-Vue.use(Radio);
-Vue.use(Modal);
+Vue.use(DatePicker)
+Vue.use(Button)
+Vue.use(Layout)
+Vue.use(Menu)
+Vue.use(Breadcrumb)
+Vue.use(Icon)
+Vue.use(Form)
+Vue.use(Input)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Radio)
+Vue.use(Modal)
+Vue.use(Tag)
 
 // 添加路由器拦截
 router.beforeEach(async (to, from, next) => {
